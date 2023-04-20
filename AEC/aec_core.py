91075d654fabc55ac2cbb9e370a9ba9e08856494
Georgii Zagoruiko <AstonMartin-one-77@outlook.com>
2023-04-20 15:03:24 +0400

    Added implementation of AEC (core part) with test code

--- AEC/aec_core.py
+++ AEC/aec_core.py
@@ -1,19 +1,109 @@
 
 import numpy as np
+import fft_Q32 as ft
 
-class ComplexBuf:
-    def __init__(self, size):
-        self.re = np.ndarray(shape=(size), dtype='int32')
-        self.re.fill(0)
-        self.im = np.ndarray(shape=(size), dtype='int32')
-        self.im.fill(0)
+class AEC_Q:
+    def __init__(self, blockSz, filterSz, stepSzQ15, lossQ15, initPwrQ15):
+        self.blockSz = int(blockSz)
+        self.filterSz = int(filterSz)
+        self.stepSz = int(stepSzQ15)
+        self.normCff = int(1 << 15) - int(lossQ15)
+        self.loss = int(lossQ15)
+        self.inBuf = np.ndarray(self.blockSz+self.filterSz, dtype='int16')
+        self.inBuf.fill(0)
+        self.wRe = np.ndarray(len(self.inBuf), dtype='int64')
+        self.wRe.fill(0)
+        self.wIm = np.ndarray(len(self.inBuf), dtype='int64')
+        self.wIm.fill(0)
+        self.normIn = np.ndarray(len(self.inBuf), dtype='int64')
+        self.normIn.fill(int(initPwrQ15))
+        self.tmpRe = np.ndarray(len(self.inBuf), dtype='int64')
+        self.tpmIm = np.ndarray(len(self.inBuf), dtype='int64') 
+        self.inFft = ft.FFT_Q32(len(self.inBuf))
+        self.ifft = ft.FFT_Q32(len(self.inBuf))
+        self.wFft = ft.FFT_Q32(len(self.inBuf))
+    
+    def echoCancel(self, micQ15, spQ15):
+        cycles = len(micQ15) // self.blockSz
+
+        for cycle in range(cycles):
+            startIdx = cycle*self.blockSz
+            endIdx = startIdx+self.blockSz
+            self.inBuf[self.filterSz:] = spQ15[startIdx:endIdx]
+            inRe, inIm = self.inFft.fft32(self.inBuf)
+
+            self.tmpRe = self.wRe*inRe - self.wIm*inIm
+            self.tmpRe >>= 15
+            self.tpmIm = self.wRe*inIm + self.wIm*inRe
+            self.tpmIm >>= 15
+            
+            echoRe, echoIm = self.ifft.ifft32(self.tmpRe, self.tpmIm)
+
+            echoRe[self.filterSz:] >>= 15
+            micQ15[startIdx:endIdx] -= echoRe[self.filterSz:]
+            self.tmpRe[self.filterSz:] = micQ15[startIdx:endIdx]*self.stepSz
+            self.tmpRe[:self.filterSz] = 0
+
+            stepRe, stepIm = self.wFft.fft32(self.tmpRe)
+
+            self.tmpRe = inRe*inRe
+            self.tmpRe += inIm*inIm
+            self.tmpRe += int(1 << 14)
+            self.tmpRe >>= 15
+            self.tmpRe *= self.normCff
+            self.tmpRe += self.normIn*self.loss
+            self.tmpRe += int(1 << 14)
+            self.normIn = self.tmpRe >> 15
+
+            self.tmpRe = stepRe*inRe + stepIm*inIm
+            self.tmpRe //= self.normIn
+            self.wRe += self.tmpRe
+            self.tmpIm = stepIm*inRe - stepRe*inIm
+            self.tmpIm //= self.normIn
+            self.wIm += self.tmpIm
+
+            self.inBuf[0:self.filterSz] = self.inBuf[self.blockSz:self.blockSz+self.filterSz]
+        
+        return micQ15
 
 
 class AEC:
-    def __init__(self, blockSz, filterSz):
+    def __init__(self, blockSz, filterSz, stepSz, initPwr, loss):
         self.blockSz = blockSz
         self.filterSz = filterSz
-        self.inputBuf = ComplexBuf(blockSz+filterSz)
+        self.stepSz = stepSz
+        self.loss = loss
+        self.normCff = 1 - loss
+        self.inBuf = np.ndarray(blockSz+filterSz, dtype="float")
+        self.inBuf.fill(0)
+        self.tmpBuf = np.ndarray(len(self.inBuf), dtype="float")
+        self.tmpBuf.fill(0)
+        self.wBuf = np.ndarray(len(self.inBuf), dtype="complex")
+        self.wBuf.fill(0)
+        self.normSp = np.ndarray(len(self.inBuf), dtype="float")
+        self.normSp.fill(initPwr)
 
-objMyClass = AEC(10,10)
-print(objMyClass.inputBuf.re, objMyClass.inputBuf.im)
+    
+    def echoCancel(self, mic, sp):
+        cycles = len(mic) // self.blockSz
+
+        for cycle in range(cycles):
+            startIdx = cycle*self.blockSz
+            endIdx = startIdx+self.blockSz
+            self.inBuf[self.filterSz:] = sp[startIdx:endIdx]
+            spCmplx = np.fft.fft(self.inBuf)
+            dsCmplx = np.fft.ifft(spCmplx*self.wBuf)
+            mic[startIdx:endIdx] -= dsCmplx.real[self.filterSz:]
+            
+            self.tmpBuf[self.filterSz:] = mic[startIdx:endIdx]*self.stepSz
+            corrCmplx = np.fft.fft(self.tmpBuf)
+            corrCmplx *= np.conj(spCmplx)
+
+            spCmplx *= np.conj(spCmplx)
+            self.normSp *= self.loss
+            self.normSp += spCmplx.real*self.normCff
+
+            self.wBuf += corrCmplx / self.normSp
+
+
+        return mic
--- AEC/fft_Q32.py
+++ AEC/fft_Q32.py
@@ -9,6 +9,88 @@ twiddle_sinCos_Q32 = np.ndarray(maxFftLine,
                                 buffer=np.array([int(np.round(maxVal_Q32*np.sin(2*np.pi*x/maxFftLine)))
                                                  for x in range(sinCosLen)]), dtype='int32')
 
+class FFT_Q32:
+    def __init__(self, size):
+        self.size = 2
+        self.pwr2 = 1
+        while self.size < size:
+            self.size *= 2
+            self.pwr2 += 1
+        self.cosShift = self.size >> 2
+        self.re = np.ndarray(self.size, dtype='int64')
+        self.im = np.ndarray(self.size, dtype='int64')
+        self.sinCos = np.ndarray(self.size+self.cosShift, 
+                                 buffer=np.array([int(np.round(1048576*np.sin(2*np.pi*x/self.size))) 
+                                                  for x in range(self.size+self.cosShift)]), dtype='int64')
+        self.bitRev = np.ndarray(self.size, dtype='int16')
+        self.bitRev.fill(0)
+        for i in range(1,self.size):
+            self.bitRev[i] = (self.bitRev[i >> 1] >> 1) | ((i & 1) << (self.pwr2 - 1))
+
+    def fft32(self, re):
+        for idx in range(len(re)):
+            self.re[self.bitRev[idx]] = re[idx]
+            self.im[self.bitRev[idx]] = 0
+        for idx in range(len(re), self.size):
+            self.re[self.bitRev[idx]] = 0
+            self.im[self.bitRev[idx]] = 0
+        
+        for depth in range(1, self.pwr2+1):
+            step = 1 << depth
+            hStep = step >> 1
+            sinStep = self.size >> depth
+
+            for idxStep in range(0, self.size, step):
+                for idx in range(hStep):
+                    sinIdx = sinStep*idx
+                    cosIdx = sinIdx+self.cosShift
+                    top = idxStep+idx
+                    bottom = top+hStep
+                    accum = self.sinCos[cosIdx]*self.re[bottom]
+                    accum -= self.sinCos[sinIdx]*self.im[bottom]
+                    accum >>= 31
+                    self.re[bottom] = self.re[top] - accum
+                    self.re[top] += accum
+                    accum = self.sinCos[cosIdx]*self.im[bottom]
+                    accum += self.sinCos[sinIdx]*self.re[bottom]
+                    accum >>= 31
+                    self.im[bottom] = self.im[top] - accum
+                    self.re[top] += accum
+
+        return self.re, self.im 
+
+    def ifft32(self, re, im):
+        for idx in range(len(re)):
+            self.re[self.bitRev[idx]] = re[idx]
+            self.im[self.bitRev[idx]] = -im[idx]
+        for idx in range(len(re), self.size):
+            self.re[self.bitRev[idx]] = 0
+            self.im[self.bitRev[idx]] = 0
+        
+        for depth in range(1, self.pwr2+1):
+            step = 1 << depth
+            hStep = step >> 1
+            sinStep = self.size >> depth
+
+            for idxStep in range(0, self.size, step):
+                for idx in range(hStep):
+                    sinIdx = sinStep*idx
+                    cosIdx = sinIdx+self.cosShift
+                    top = idxStep+idx
+                    bottom = top+hStep
+                    accum = self.sinCos[cosIdx]*self.re[bottom]
+                    accum -= self.sinCos[sinIdx]*self.im[bottom]
+                    accum >>= 31
+                    self.re[bottom] = self.re[top] - accum
+                    self.re[top] += accum
+                    accum = self.sinCos[cosIdx]*self.im[bottom]
+                    accum += self.sinCos[sinIdx]*self.re[bottom]
+                    accum >>= 31
+                    self.im[bottom] = self.im[top] - accum
+                    self.re[top] += accum
+
+        return self.re, self.im
+
 def fft_Q32(re, bitRev, pwr2):
     outSz = len(bitRev)
     outRe = np.ndarray(shape=(outSz), dtype='int32')
