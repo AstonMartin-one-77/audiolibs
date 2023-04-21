3482d955046e543626327665a44800e87d32714f
Georgii Zagoruiko <AstonMartin-one-77@outlook.com>
2023-04-21 16:18:13 +0400

    Added align AEC implementation. TODO: add exceptions and UT

--- AEC/aec_core.py
+++ AEC/aec_core.py
@@ -82,10 +82,12 @@ class AEC:
         self.wBuf.fill(0)
         self.normSp = np.ndarray(len(self.inBuf), dtype="float")
         self.normSp.fill(initPwr)
-
     
     def echoCancel(self, mic, sp):
         cycles = len(mic) // self.blockSz
+
+        if cycles == 0:
+            return
 
         for cycle in range(cycles):
             startIdx = cycle*self.blockSz
@@ -107,3 +109,123 @@ class AEC:
 
 
         return mic
+
+class RingBuffer:
+    def __init__(self, bufSz):
+        self.buf = np.ndarray(bufSz, dtype="float")
+        self.wIdx = 0
+        self.rIdx = 0
+    
+    def set(self, frame):
+
+        if self.wIdx >= self.rIdx:
+            endSpace = len(self.buf)-self.wIdx
+            free = endSpace+self.rIdx-1
+            if endSpace > len(frame):
+                self.buf[self.wIdx:self.wIdx+len(frame)] = frame
+                self.wIdx += len(frame)
+            elif free >= len(frame):
+                self.buf[self.wIdx:] = frame[:endSpace]
+                self.buf[:len(frame)-endSpace] = frame[endSpace:]
+                self.wIdx = len(frame)-endSpace
+            else:
+                # TODO: exception
+                pass
+        else:
+            free = self.rIdx-self.wIdx-1
+            if free >= len(frame):
+                self.buf[self.wIdx:self.wIdx+len(frame)] = frame
+                self.wIdx += len(frame)
+            else:
+                # TODO: exception
+                pass
+    
+    def getPlenum(self):
+        if self.wIdx >= self.rIdx:
+            return self.wIdx-self.rIdx
+        else:
+            return len(self.buf)-self.rIdx+self.wIdx
+    
+
+    def get(self, size):
+        if self.wIdx >= self.rIdx:
+            busy = self.wIdx-self.rIdx
+            if busy < size:
+                return
+            else:
+                res = np.copy(self.buf[self.rIdx:self.rIdx+size])
+                self.rIdx += size
+                return res
+        else:
+            endBusy = len(self.buf)-self.rIdx
+            busy = endBusy+self.wIdx
+            if endBusy > size:
+                res = np.copy(self.buf[self.rIdx:self.rIdx+size])
+                self.rIdx += size
+                return res
+            elif busy >= size:
+                res = np.ndarray(size, dtype="float")
+                res[:endBusy] = self.buf[self.rIdx:]
+                res[endBusy:] = self.buf[:size-endBusy]
+                self.rIdx = size-endBusy
+                return res
+
+
+class AEC_Align:
+    def __init__(self):
+        self.spRingBuf = RingBuffer(48000) # 1 sec delay if it has 48kHz
+        self.micRingBuf = RingBuffer(48000) # 1 sec delay if it has 48kHz
+        self.blockSz = 4096
+        self.filterSz = 4096
+        self.alignStepSz = 128
+        self.aec = AEC(self.blockSz, self.filterSz, 0.032, 0.01, 0.98)
+        self.micBuf = np.ndarray(self.filterSz+self.alignStepSz, dtype="float")
+        self.isAlign = False
+        self.cnvVal = 0
+
+    def setMic(self, mic):
+        if self.isAlign == False:
+            cycles = len(mic) // self.alignStepSz
+            for cycle in range(cycles):
+                start = cycle*self.alignStepSz
+                end = start+self.alignStepSz
+                self.micBuf[self.filterSz:] = mic[start:end]
+                cnvRes = np.convolve(self.micBuf[self.alignStepSz:], self.spRingBuf.buf[:self.filterSz],"valid")
+                crrCnv = cnvRes[0] / self.filterSz
+                if crrCnv > 0.2:
+                    if crrCnv >= self.cnvVal:
+                        self.cnvVal = crrCnv
+                        self.micBuf[:self.filterSz] = self.micBuf[self.alignStepSz:]
+                    else:
+                        self.micRingBuf.set(self.micBuf)
+                        self.micRingBuf.set(mic[end:])
+                        self.isAlign = True
+                        break
+                else:
+                    self.micBuf[:self.filterSz] = self.micBuf[self.alignStepSz:]
+            
+            remain = len(mic)-cycles*self.alignStepSz
+            if self.isAlign == False:
+                self.micBuf[self.filterSz:self.filterSz+remain] = mic[len(mic)-remain:]
+                cnvRes = np.convolve(self.micBuf[remain:remain+self.filterSz], self.spRingBuf.buf[:self.filterSz],"valid")
+                crrCnv = cnvRes[0] / self.filterSz
+                if crrCnv > 0.2:
+                    if crrCnv >= self.cnvVal:
+                        self.cnvVal = crrCnv
+                        self.micBuf[:self.filterSz] = self.micBuf[remain:remain+self.filterSz]
+                    else:
+                        self.micRingBuf.set(self.micBuf)
+                        self.micRingBuf.set(mic[end:])
+                        self.isAlign = True
+                else:
+                    self.micBuf[:self.filterSz] = self.micBuf[remain:remain+self.filterSz]
+
+    def setSp(self, sp):
+        self.spRingBuf.set(sp)
+
+    def echoCancel(self):
+        if self.isAlign == True:
+            sp = self.spRingBuf.get(self.blockSz)
+            mic = self.micRingBuf.get(self.blockSz)
+            if sp != None and mic != None:
+                self.aec.echoCancel(mic, sp)
