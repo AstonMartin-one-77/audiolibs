c418cf7d555afc54529b8d42a6aa18d2a6546270
Georgii Zagoruiko <astonmartin-one-77@outlook.com>
2023-11-13 11:58:57 +0000

    feat: Simple TensorFlow like implementation of layer-based Neural Network
    Debug is finished for BaseOptimizer()
    
    TODO: debug Adam optimizer

--- MathML/DenseLayer.py
+++ MathML/DenseLayer.py
@@ -27,10 +27,10 @@ class BaseOptimizer:
         self.lrate = lrate
     
     def dW(self, g:np.array):
-        return -g*self.lrate
+        return g*self.lrate
     
     def db(self, g:np.array):
-        return -np.sum(g, axis=1, keepdims=True)*self.lrate
+        return np.sum(g, axis=1, keepdims=True)*self.lrate
 
 class InputLayer(BaseLayer):
     def __init__(self, units: int):
@@ -65,12 +65,8 @@ class DenseLayer(BaseLayer):
     def backward(self, d:np.array):
         dAdZ = d*self.dG(self.saveZ)
         dZdA = dAdZ.T @ self.W
-        # self.W += self.optimizer.dW(dAdZ @ self.prevA.T)
-        dW = dAdZ @ self.prevA.T / 100
-        self.W -= dW * 3.1
-        # self.b += self.optimizer.db(dAdZ)
-        db = np.sum(dAdZ, axis=1, keepdims=True) / 100
-        self.b -= db * 3.1
+        self.W += self.optimizer.dW(dAdZ @ self.prevA.T)
+        self.b += self.optimizer.db(dAdZ)
         self.bLayer.backward(dZdA.T)
 
     def forward(self, aVal:np.array):
@@ -90,9 +86,15 @@ class DenseLayer(BaseLayer):
     def setOptimizer(self, optimizer:BaseOptimizer):
         self.optimizer = optimizer
 
-    def compile(self):
-        self.W = np.random.randn(self.units, self.bLayer.getUnits()) / 2
-        self.b = np.random.randn(self.units, 1) / 2
+    def compile(self, W:np.array = None, b:np.array = None):
+        if W is not None:
+            self.W = W
+        else:
+            self.W = np.random.randn(self.units, self.bLayer.getUnits()) / 2
+        if b is not None:
+            self.b = b
+        else:
+            self.b = np.random.randn(self.units, 1) / 2
 
 class AdamOptimizer(BaseOptimizer):
     def __init__(self, lrate:float = 0.001, b1:float = 0.9, b2:float = 0.999):
@@ -132,6 +134,13 @@ if __name__ == "__main__":
     dl2.setOptimizer(BaseOptimizer(lrate=0.031))
     output.setOptimizer(BaseOptimizer(lrate=0.031))
 
+    # NW = NeuralNetwork([(1,5),(5,4),(4,2)])
+    # w0 = np.array([[ 0.06332294],       [-1.64162638],       [ 0.13457888],       [ 0.4365871 ],       [ 0.79491503]])
+    # b0 = np.array([[-0.13764146],       [ 0.11569728],       [-0.51257726],       [-0.31815902],       [-0.15085569]])
+    # w1 = np.array([[ 0.54963372, -0.33513557, -0.24486069,  0.12902891, -0.4986361 ],       [ 0.03465819,  0.01338351, -0.10258007,  0.86133015,  0.73636143],       [ 0.03382981,  0.19389837, -0.02179771, -0.34117452, -0.37477451],       [-0.79792423,  0.86582359,  0.25982899, -0.25303926,  0.44011548]])
+    # b1 = np.array([[-0.64159605],       [ 0.25569778],       [-0.37210437],       [-0.0923177 ]])
+    # w2 = np.array([[ 0.73193099,  0.11739459, -0.7096986 ,  0.45298538],       [ 0.28562191,  0.24134889, -0.0294186 , -0.61084058]])
+    # b2 = np.array([[-0.28801579],       [ 0.11429052]])
     dl1.compile()
     dl2.compile()
     output.compile()
@@ -142,13 +151,12 @@ if __name__ == "__main__":
 
     for i in range(40000):
         res = input.forward(t)
-        output.backward(2*(res-y))
+        output.backward(2*(y-res))
 
     print(np.sum((input.forward(t)-y)**2)/len(y))
 
     t_n,y_n = NNg.lissajous_curve(N=150)
     y_pred = input.forward(t_n)
-    y_pred = y_pred.T
 
     fig,ax = plt.subplots(figsize=(8, 8))
     fig.set_facecolor("slategray")
